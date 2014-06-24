95d480318d48d8cab863937eb5592b38adb909df
jerearista <jere@arista.com>
2014-06-24 16:30:26 -0400

    This is my new commit.

--- listInts.py
+++ listInts.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 
+"""
+Created on 10 June 2014
+@author: Jere Julian
+"""
+
 from jsonrpclib import Server
 import pprint
+import Foo
 
-switch = Server( "https://arista:arista@10.10.10.11/command-api" )
+if __name__ == '__main__':
+    switch = Server( "https://arista:arista@10.10.10.11/command-api" )
 
-response = switch.runCmds( 1, ["show interfaces description"] )
+    response = switch.runCmds( 1, ["show interfaces description"] )
 
-#print "The switch's system MAC addess is", response[0]["systemMacAddress"]
+    #print "The switch's system MAC addess is", response[0]["systemMacAddress"]
 
-#pp = pprint.PrettyPrinter()
-#pp.pprint(response[0])
-pprint.pprint(response[0])
+    #pp = pprint.PrettyPrinter()
+    #pp.pprint(response[0])
+    pprint.pprint(response[0])
 
 
-print "The switch's system Management description is '{0}'".format(response[0]["interfaceDescriptions"]["Management1"]["description"])
+    print "The switch's system Management description is '{0}'".format(response[0]["interfaceDescriptions"]["Management1"]["description"])
